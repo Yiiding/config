ca199d9947f9f98932ebe7fad344a142048b9ac8
Julian Berrer <julianberrer2008@gmail.com>
2025-09-01 13:46:07 +0200

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+chcp 65001 > $null
+
+# oh-my-posh
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\Yiiding\config\mytheme.omp.json" | Invoke-Expression
+# man könnte noch status hinzufügen (Herz rot|grün)
+# oh-my-posh init pwsh --config 'jandedobbeleer' | Invoke-Expression
